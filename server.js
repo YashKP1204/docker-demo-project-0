57016632512e7a495a83ac3672d805dd8077b903
YashKP1204 <yashprajapati121204@gmail.com>
2025-06-11 17:12:43 +0530

    second push

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const studentRoutes = require('./routes/studentRoutes');
+const studentRoutes = require('./routes/studentRotues');
 
 dotenv.config();
 const app = express();
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch(err => console.error(err));
